7dc866528bb10af8c8a070b5d4d950e8599a8eaf
Khepri <25828025+KDA-dev@users.noreply.github.com>
2022-10-10 17:28:05 +0500

    Update cpplabs.cpp

--- cpplabs.cpp
+++ cpplabs.cpp
@@ -47,10 +47,10 @@ stackm<T>::~stackm() {
 
 template<typename T>
 void stackm<T>::resize_if_needed() {
-    if(count == capacity) {
+    if (count == capacity) {
         capacity *= 2;
         T* new_data = new T[capacity];
-        for(size_t i = 0; i < count; i++)
+        for (size_t i = 0; i < count; i++)
             new_data[i] = data[i];
         delete[] data;
         data = new_data;
@@ -116,30 +116,27 @@ stackc<T>::stackc() {
 
 template<typename T>
 stackc<T>::~stackc() {
-    delete topnode;
+    while (count != 0)
+        pop();
 }
 
 template<typename T>
 void stackc<T>::push(const T& elem) {
-    node *new_node = new node;
-    new_node->data = elem;
-    new_node->next = topnode;
-    topnode = new_node;
+    node* new_node = new node{ elem, top };
     ++count;
 }
 
 template<typename T>
 void stackc<T>::push(T&& elem) {
-    node *new_node = new node;
-    new_node->data = elem;
-    new_node->next = top;
-    topnode = new_node;
+    node* new_node = new node{ elem, top };
     ++count;
 }
 
 template<typename T>
 void stackc<T>::pop() {
-    topnode = topnode->next;
+    node* new_node = topnode->next;
+    delete topnode;
+    topnode = new_node;
     --count;
 }
 
@@ -160,19 +157,17 @@ size_t stackc<T>::size() const {
 
 bool is_bracket_sequence_balanced(const string& seq) {
     stackm<char> st;
-    map<char, char> ctrpart = {{ '(', ')'}, { '[', ']'}, { '{', '}'}};
-    for(int i = 0; i < seq.size(); i++) {
-        if(ctrpart.count(seq[i]) != 0) {
+    map<char, char> ctrpart = { { '(', ')'}, { '[', ']'}, { '{', '}'} };
+    for (int i = 0; i < seq.size(); i++) {
+        if (ctrpart.count(seq[i]) != 0) {
             st.push(seq[i]);
-            int z = 0;
         }
         else {
             char c = st.top();
-            if(ctrpart[st.top()] == seq[i])
+            if (ctrpart[st.top()] == seq[i])
                 st.pop();
             else
                 return false;
-            int z = 0;
         }
     }
     return st.size() == 0;
@@ -181,39 +176,42 @@ bool is_bracket_sequence_balanced(const string& seq) {
 template<typename T>
 bool does_vector_contain_another(const vector<T>& vec, const vector<T>& another) {
     stackc<T> st;
-    for(int i = another.size() - 1; i >=0; i--)
+    for (int i = another.size() - 1; i >= 0; i--)
         st.push(another[i]);
-    for(int i = 0; i < vec.size(); i++) {
-        if(st.size() != 0 && vec[i] == st.top())
+    for (int i = 0; i < vec.size(); i++) {
+        if (st.size() != 0 && vec[i] == st.top())
             st.pop();
     }
     return st.size() == 0;
 }
 
-vector<int> find_euler_path(vector<vector<int>>& G) {
+vector<int> find_euler_path(vector<vector<pair<int, int>>>& G, int edge_cnt) {
+    //every edge should have a sequential number attached
     stackm<int> st;
     vector<int> ans;
+    vector<bool> visited(edge_cnt, false);
+    vector<int> available(G.size(), 0);
 
     st.push(0);
-    while(st.size() != 0) {
+    while (st.size() != 0) {
         int v = st.top();
-        if(G[v].size() == 0) {
+        if (available[v] == G[v].size()) {
             ans.push_back(v);
             st.pop();
         }
         else {
-            int w = G[v].back();
-            G[v].pop_back();
-            for(int i = 0; i < G[w].size(); i++) {
-                if(G[w][i] == v) {
-                    swap(G[w][i], G[w].back());
-                    G[w].pop_back();
-                    break;
-                }
-            }
+            int w = G[v][available[v]].first;
+
+            visited[G[v][available[v]].second] = true;
+            while (available[v] < G[v].size() && visited[G[v][available[v]].second])
+                available[v]++;
+            while (available[w] < G[w].size() && visited[G[w][available[w]].second])
+                available[w]++;
+
             st.push(w);
         }
     }
+
     return ans;
 }
 
@@ -224,7 +222,7 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    
+
     cout << (is_bracket_sequence_balanced("([()()]())") ? "YES\n" : "NO\n");
-    cout << (does_vector_contain_another<int>({1, 2, 3, 4, 5, 6}, {1, 3, 5}) ? "YES\n" : "NO\n");
+    cout << (does_vector_contain_another<int>({ 1, 2, 3, 4, 5, 6 }, { 1, 3, 5 }) ? "YES\n" : "NO\n");
 }
