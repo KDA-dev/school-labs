72a1d221108c23e135968b7373f0d7b7534884db
Khepri <25828025+KDA-dev@users.noreply.github.com>
2022-10-31 00:45:19 +0500

    Update cpplabs.cpp

--- cpplabs.cpp
+++ cpplabs.cpp
@@ -31,7 +31,13 @@ public:
     T& top();
     const T& top() const;
     size_t size() const;
+    T& prev();
 };
+
+template<typename T>
+T& stackm<T>::prev() {
+    return data[count - 2];
+}
 
 template<typename T>
 stackm<T>::stackm() {
@@ -208,36 +214,6 @@ bool does_vector_contain_another(const vector<T>& vec, const vector<T>& another)
     return st.size() == 0;
 }
 
-vector<int> find_euler_path(vector<vector<pair<int, int>>>& G, int edge_cnt) {
-    //every edge should have a sequential number attached
-    stackm<int> st;
-    vector<int> ans;
-    vector<bool> visited(edge_cnt, false);
-    vector<int> available(G.size(), 0);
-
-    st.push(0);
-    while (st.size() != 0) {
-        int v = st.top();
-        if (available[v] == G[v].size()) {
-            ans.push_back(v);
-            st.pop();
-        }
-        else {
-            int w = G[v][available[v]].first;
-
-            visited[G[v][available[v]].second] = true;
-            while (available[v] < G[v].size() && visited[G[v][available[v]].second])
-                available[v]++;
-            while (available[w] < G[w].size() && visited[G[w][available[w]].second])
-                available[w]++;
-
-            st.push(w);
-        }
-    }
-
-    return ans;
-}
-
 typedef vector<vector<int>> Graph;
 
 // Вершины графа должны быть занумерованы одинаково, иначе NP
@@ -313,7 +289,176 @@ void sort_stack(stackm<T>& st) {
         st.push(sec.top());
         sec.pop();
     }
-} 
+}
+
+struct point {
+    double x, y;
+
+    point operator+(const point &other) const {
+        return { x + other.x, y + other.y };
+    }
+    point operator-(const point &other) const {
+        return { x - other.x, y - other.y };
+    }
+    point operator*(const int &other) const {
+        return { x * other, y * other };
+    }
+    point operator/(const int &other) const {
+        return { x / other, y / other };
+    }
+    double operator*(const point &other) const {
+        return x * other.x + y * other.y;
+    }
+    double operator^(const point &other) const {
+        return x * other.y - other.x * y;
+    }
+    double len() const {
+        return sqrt(x * x + y * y);
+    }
+    point norm() const {
+        return {x / len(), y / len()};
+    }
+};
+
+double oriented_triangle_area(point p1, point p2, point p3) {
+    return ((p2 - p1) ^ (p3 - p2)) / 2.;
+}
+
+// Задание 1: Эйлеров Путь
+vector<int>& find_euler_path(vector<vector<pair<int, int>>>& G, int edge_cnt) {
+    //every edge should have a sequential number attached
+    stackm<int> st;
+    vector<int> ans;
+    vector<bool> visited(edge_cnt, false);
+
+    for(int i = 0; i < G.size(); i++) {
+        if(G[i].size() % 2 == 1) {
+            st.push(i);
+            break;
+        }
+    }
+    if(st.size() == 0)
+        st.push(0);
+    
+    while (st.size() != 0) {
+        int v = st.top();
+        bool found_edge = false;
+        for(int i = 0; i < G[v].size(); i++) {
+            if(!visited[G[v][i].second]) {
+                st.push(i);
+                visited[G[v][i].second] = true;
+                found_edge = true;
+                break;
+            }
+        }
+        if(!found_edge) {
+            st.pop();
+            ans.push_back(v);
+        }
+    }
+
+    return ans;
+}
+
+// Задание 2: Выпуклая оболочка
+set<int>& graham(const vector<point>& v) {
+    vector<int> idx(v.size());
+    for(int i = 0; i < v.size(); i++)
+        idx[i] = i;
+    for(int i = 1; i < v.size(); i++) {
+        if(v[idx[i]].y < v[idx[0]].y || (v[idx[i]].y == v[idx[0]].y && v[idx[i]].x > v[idx[0]].x))
+            swap(idx[i], idx[0]);
+    }
+    point v0 = v[idx[0]];
+    sort(idx.begin() + 1, idx.end(), [v0, v](int a, int b) {
+        if(((v[a] - v0) ^ (v[b] - v0)) == 0)
+            return (v[a] - v0).x < (v[b] - v0).x;
+        return ((v[a] - v0) ^ (v[b] - v0)) > 0;
+    });
+    
+    stackm<int> p;
+    p.push(idx[0]);
+    p.push(idx[1]);
+    for(int i = 2; i < v.size(); i++) {
+        while(((v[p.top()] - v[p.prev()]) ^ (v[idx[i]] - v[p.top()])) <= 0)
+            p.pop();
+        p.push(idx[i]);
+    }
+    set<int> ans;
+    while(p.size() != 0) {
+        ans.insert(p.top());
+        p.pop();
+    }
+    return ans;
+}
+
+int count_rings(vector<point>& v) {
+    vector<point> v2;
+    int ans = 0;
+    while(!v.empty()) {
+        v2.clear();
+        set<int> discards = graham(v);
+        for(int i = 0; i < v.size(); i++)
+            if(discards.count(i) == 0)
+                v2.push_back(v[i]);
+        v = v2;
+        ans++;
+    }
+    return ans;
+}
+
+template<typename T, template<typename> typename stack>
+void turnaround(stack<T>& target) {
+    vector<T> tmp;
+    while(target.size() != 0) {
+        tmp.push_back(target.top());
+        target.pop();
+    }
+    for(T& a : tmp)
+        target.push(a);
+}
+template<typename T, template<typename> typename stack>
+void merge(stack<T>& s1, stack<T>& s2, stack<T>& target) {
+    while(s1.size() != 0 && s2.size() != 0) {
+        if(s1.top() > s2.top()) {
+            target.push(s1.top());
+            s1.pop();
+        }
+        else {
+            target.push(s2.top());
+            s2.pop();
+        }
+    }
+    while(s1.size() != 0) {
+        target.push(s1.top());
+        s1.pop();
+    }
+    while(s2.size() != 0) {
+        target.push(s2.top());
+        s2.pop();
+    }
+}
+template<typename T, template<typename> typename stack>
+void merge_sort(stack<T>& s) {
+    int half = s.size() / 2;
+    if(half == 0)
+        return;
+
+    stack<T> left, right;
+    while(s.size() > half) {
+        left.push(s.top());
+        s.pop();
+    }
+    while(s.size() > 0) {
+        right.push(s.top());
+        s.pop();
+    }
+
+    merge_sort(left);
+    merge_sort(right);
+    merge(left, right, s);
+    turnaround(s);
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -331,7 +476,7 @@ int main() {
     st.push(4);
     st.push(5);
     st.push(2);
-    sort_stack(st);
+    merge_sort(st);
     while (st.size() != 0)
     {
         cout << st.top() << ' ';
