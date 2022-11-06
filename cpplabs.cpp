d3df258b9b3cdf9c96779d362b415f1fdbbd5466
Khepri <25828025+KDA-dev@users.noreply.github.com>
2022-11-06 23:45:38 +0500

    Update cpplabs.cpp

--- cpplabs.cpp
+++ cpplabs.cpp
@@ -32,6 +32,7 @@ public:
     const T& top() const;
     size_t size() const;
     T& prev();
+    T& ptop();
 };
 
 template<typename T>
@@ -84,6 +85,10 @@ template<typename T>
 T& stackm<T>::top() {
     return data[count - 1];
 }
+template<typename T>
+T& stackm<T>::ptop() {
+    return data[--count];
+}
 
 template<typename T>
 const T& stackm<T>::top() const {
@@ -95,369 +100,224 @@ size_t stackm<T>::size() const {
     return count;
 }
 
-template<typename T>
-class stackc {
-private:
-    struct node {
-        T data;
-        node* next;
-    } *topnode;
-    size_t count;
-public:
-    stackc();
-    ~stackc();
-    void push(const T& elem);
-    void push(T&& elem);
-    void pop();
-    T& top();
-    const T& top() const;
-    size_t size() const;
+enum class term_type { var, cnst, opbr, clbr, op, fun };
+struct term
+{
+    string name;
+    term_type type;
 };
+ 
+map<string, int> priority;
+map<string, double> vars;
+map<string, function<double(stackm<double>& in)>> funs;
+ 
+void build_postfixcalc_prereqs()
+{
+    priority["+"] = 5;
+    priority["-"] = 5;
+    priority["u-"] = 20;
+    priority["*"] = 10;
+    priority["/"] = 10;
+    priority["^"] = 15;
+    priority["!"] = 15;
 
-template<typename T>
-stackc<T>::stackc() {
-    topnode = nullptr;
-    count = 0;
-}
+    vars["abc"] = 1;
+    vars["x"] = 2;
+    vars["y"] = 3;
+    vars["z"] = 4;
+    vars["n"] = 5;
+    vars["a"] = 6;
+    vars["b"] = 7;
 
-template<typename T>
-stackc<T>::~stackc() {
-    while (count != 0)
-        pop();
-}
-
-template<typename T>
-void stackc<T>::push(const T& elem) {
-    topnode = new node{ elem, topnode };
-    ++count;
-}
-
-template<typename T>
-void stackc<T>::push(T&& elem) {
-    topnode = new node{ elem, topnode };
-    ++count;
-}
-
-template<typename T>
-void stackc<T>::pop() {
-    node* new_node = topnode->next;
-    delete topnode;
-    topnode = new_node;
-    --count;
-}
-
-template<typename T>
-T& stackc<T>::top() {
-    return topnode->data;
-}
-
-template<typename T>
-const T& stackc<T>::top() const {
-    return topnode->data;
-}
-
-template<typename T>
-size_t stackc<T>::size() const {
-    return count;
-}
-
-template<typename T>
-bool isVecPermutationOfOther(const vector<T> &vec, const vector<T> &other) {
-    if(vec.size() != other.size())
-        return false;
-    vector<bool> foundElems(vec.size(), false);
-    for(int i = 0; i < vec.size(); i++) {
-        bool elem_found = false;
-        for(int j = 0; j < other.size(); j++) {
-            if(foundElems[i])
-                continue;
-            if(vec[i] == other[j]) {
-                elem_found = true;
-                foundElems[j] = true;
-                break;
-            }
+    funs.emplace("ln", [](stackm<double>& in) -> double {
+        return log(in.ptop());
+    });
+    funs.emplace("sin", [](stackm<double>& in) -> double {
+        return sin(in.ptop());
+    });
+    funs.emplace("!", [](stackm<double>& in) -> double  {
+        double n = in.ptop(), ans = 1;
+        int z = 0;
+        while(n > 1) {
+            ans *= n;
+            n--;
         }
-        if(!elem_found)
-            return false;
+        return ans;
+    });
+    funs.emplace("+", [](stackm<double>& in) -> double {
+        double b = in.ptop(), a = in.ptop();
+        return a + b;
+    });
+    funs.emplace("-", [](stackm<double>& in) -> double {
+        double b = in.ptop(), a = in.ptop();
+        return a - b;
+    });
+    funs.emplace("*", [](stackm<double>& in) -> double {
+        double b = in.ptop(), a = in.ptop();
+        return a * b;
+    });
+    funs.emplace("/", [](stackm<double>& in) -> double {
+        double b = in.ptop(), a = in.ptop();
+        return a + b;
+    });
+    funs.emplace("^", [](stackm<double>& in) -> double {
+        double b = in.ptop(), a = in.ptop();
+        return pow(a, b);
+    });
+    funs.emplace("u-", [](stackm<double>& in) -> double {
+        return -in.ptop();
+    });
+}
+ 
+const string delimiters = "~!@#$%^&*()+-/ ";
+ 
+bool is_delimiter(char ch)
+{
+    return delimiters.find(ch) != string::npos;
+}
+ 
+vector<term> create_terms_from_expression(const string& expr)
+{
+    vector<term> res;
+    int i = 0;
+    while (i < expr.size())
+    {
+        if (expr[i] == ' ') { ++i; continue; }
+        string name = string(1, expr[i]);
+        if(!is_delimiter(expr[i]))
+            while (i + 1 < expr.size() && !is_delimiter(expr[i + 1]))
+                name += expr[++i];
+        ++i;
+        res.push_back({ name,term_type::op });
     }
-    return true;
-}
-
-bool is_bracket_sequence_balanced(const string& seq) {
-    stackm<char> st;
-    string op_br = "{[(<", cl_br = "}])>";
-    for (int i = 0; i < seq.size(); i++) {
-        auto idx = op_br.find(seq[i]);
-        if (idx != string::npos) {
-            st.push(seq[i]);
+    for (int i = 0; i < res.size(); ++i)
+    {
+        char ch = res[i].name[0];
+        if (isdigit(ch))
+            res[i].type = term_type::cnst;
+        else if (ch=='(')
+            res[i].type = term_type::opbr;
+        else if (ch == ')')
+            res[i].type = term_type::clbr;
+        else if (ch == '-') {
+            if(i == 0 || res[i - 1].type == term_type::opbr || res[i - 1].type == term_type::op)
+                res[i].name = "u-";
         }
-        else {
-            idx = cl_br.find(seq[i]);
-            if (st.size() != 0 && op_br[idx] == st.top())
-                st.pop();
+        else if (isalpha(ch))
+            if(i+1<res.size()&&res[i+1].name=="(")
+                res[i].type = term_type::fun;
             else
-                return false;
+                res[i].type = term_type::var;
+    }
+    return res;
+}
+ 
+vector<term> create_postfix_from_expression(const vector<term>& terms)
+{
+    vector<term> res;
+    stackm<term> s;
+    for (const auto& t : terms)
+    {
+        switch (t.type)
+        {
+        case term_type::var:
+        case term_type::cnst: 
+            res.push_back(t); 
+            break;
+        case term_type::opbr: 
+        case term_type::fun: 
+            s.push(t); 
+            break;
+        case term_type::clbr:
+            while (s.top().type != term_type::opbr)
+            {
+                res.push_back(s.top());
+                s.pop();
+            }
+            s.pop();
+            break;
+        case term_type::op:
+            while (s.size() && s.top().type != term_type::opbr)
+                if (s.top().type == term_type::fun ||
+                    priority[s.top().name] >= priority[t.name])
+                {
+                    res.push_back(s.top());
+                    s.pop();
+                }
+                else
+                    break;
+            s.push(t);
         }
     }
-    return st.size() == 0;
+    while (s.size())
+    {
+        res.push_back(s.top());
+        s.pop();
+    }
+    return res;
 }
 
-template<typename T>
-bool does_vector_contain_another(const vector<T>& vec, const vector<T>& another) {
-    stackc<T> st;
-    for (int i = another.size() - 1; i >= 0; i--)
-        st.push(another[i]);
-    for (int i = 0; i < vec.size(); i++) {
-        if (st.size() != 0 && vec[i] == st.top())
-            st.pop();
+double calculate_postfix(const vector<term>& terms) {
+    stackm<double> calc;
+    for(auto t : terms) {
+        int z = 0;
+        switch(t.type) {
+            case term_type::var:
+                calc.push(vars[t.name]);
+                break;
+            case term_type::cnst:
+                calc.push(atof(t.name.c_str()));
+                break;
+            case term_type::op:
+            case term_type::fun:
+                double res = funs[t.name](calc);
+                calc.push(res);
+                break;
+        }
     }
-    return st.size() == 0;
+    return calc.top();
 }
+#define _DEBUG
 
 typedef vector<vector<int>> Graph;
-
-// Вершины графа должны быть занумерованы одинаково, иначе NP
-bool is_graph_same_as_another(const Graph& g, const Graph& another) {
-    if(g.size() != another.size())
-        return false;
-    for(int i = 0; i < g.size(); i++)
-        if(!isVecPermutationOfOther(g[i], another[i]))
-            return false;
-    return true;
-}
-bool is_graph_subset_of_another(Graph& g, Graph& another) {
-    if(g.size() > another.size())
-        return false;
-    for(int i = 0; i < g.size(); i++) {
-        sort(g[i].begin(),g[i].end());
-        sort(another[i].begin(), another[i].end());
-        if(!does_vector_contain_another(another[i], g[i]))
-            return false;
-    }
-    return true;
-}
-
-int find_only_nonsequence_bracket(const string& seq) {
-    stackm<pair<char, int>> st;
-    string op_br = "{[(<", cl_br = "}])>";
-    int sus1 = -1, sus2 = -1;
-    for (int i = 0; i < seq.size(); i++) {
-        auto idx = op_br.find(seq[i]);
-        if (idx != string::npos) {
-            st.push(mp(seq[i], idx));
-        }
-        else {
-            idx = cl_br.find(seq[i]);
-            if (st.size() != 0 && op_br[idx] == st.top().first)
-                st.pop();
-            else if(sus1 == -1) {
-                sus1 = st.top().second;
-                sus2 = i;
-                --i;
-            }
-            else
-                return sus2;
-        }
-    }
-    if(sus1 == -1)
-        return st.top().second;
-    if(st.size() != 0)
-        return sus2;
-    return sus1;
-}
-
-template<typename T>
-void sort_stack(stackm<T>& st) {
-    stackm<T> sec, buffer;
-    while (st.size() != 0)
-    {
-        while (sec.size() != 0 && sec.top() > st.top())
-        {
-            buffer.push(sec.top());
-            sec.pop();
-        }
-        sec.push(st.top());
-        st.pop();
-        while (buffer.size() != 0)
-        {
-            sec.push(buffer.top());
-            buffer.pop();
-        }
-    }
-    while (sec.size() != 0)
-    {
-        st.push(sec.top());
-        sec.pop();
-    }
-}
-
-struct point {
-    double x, y;
-
-    point operator+(const point &other) const {
-        return { x + other.x, y + other.y };
-    }
-    point operator-(const point &other) const {
-        return { x - other.x, y - other.y };
-    }
-    point operator*(const int &other) const {
-        return { x * other, y * other };
-    }
-    point operator/(const int &other) const {
-        return { x / other, y / other };
-    }
-    double operator*(const point &other) const {
-        return x * other.x + y * other.y;
-    }
-    double operator^(const point &other) const {
-        return x * other.y - other.x * y;
-    }
-    double len() const {
-        return sqrt(x * x + y * y);
-    }
-    point norm() const {
-        return {x / len(), y / len()};
-    }
-};
-
-double oriented_triangle_area(point p1, point p2, point p3) {
-    return ((p2 - p1) ^ (p3 - p2)) / 2.;
-}
-
-// Задание 1: Эйлеров Путь
-vector<int>& find_euler_path(vector<vector<pair<int, int>>>& G, int edge_cnt) {
-    //every edge should have a sequential number attached
-    stackm<int> st;
+vector<int> to_proofer(Graph& g) {
     vector<int> ans;
-    vector<bool> visited(edge_cnt, false);
-
-    for(int i = 0; i < G.size(); i++) {
-        if(G[i].size() % 2 == 1) {
-            st.push(i);
-            break;
-        }
-    }
-    if(st.size() == 0)
-        st.push(0);
-    
-    while (st.size() != 0) {
-        int v = st.top();
-        bool found_edge = false;
-        for(int i = 0; i < G[v].size(); i++) {
-            if(!visited[G[v][i].second]) {
-                st.push(i);
-                visited[G[v][i].second] = true;
-                found_edge = true;
-                break;
-            }
-        }
-        if(!found_edge) {
-            st.pop();
-            ans.push_back(v);
-        }
-    }
-
-    return ans;
-}
-
-// Задание 2: Выпуклая оболочка
-set<int>& graham(const vector<point>& v) {
-    vector<int> idx(v.size());
-    for(int i = 0; i < v.size(); i++)
-        idx[i] = i;
-    for(int i = 1; i < v.size(); i++) {
-        if(v[idx[i]].y < v[idx[0]].y || (v[idx[i]].y == v[idx[0]].y && v[idx[i]].x > v[idx[0]].x))
-            swap(idx[i], idx[0]);
-    }
-    point v0 = v[idx[0]];
-    sort(idx.begin() + 1, idx.end(), [v0, v](int a, int b) {
-        if(((v[a] - v0) ^ (v[b] - v0)) == 0)
-            return (v[a] - v0).x < (v[b] - v0).x;
-        return ((v[a] - v0) ^ (v[b] - v0)) > 0;
-    });
-    
-    stackm<int> p;
-    p.push(idx[0]);
-    p.push(idx[1]);
-    for(int i = 2; i < v.size(); i++) {
-        while(((v[p.top()] - v[p.prev()]) ^ (v[idx[i]] - v[p.top()])) <= 0)
-            p.pop();
-        p.push(idx[i]);
-    }
-    set<int> ans;
-    while(p.size() != 0) {
-        ans.insert(p.top());
-        p.pop();
+    int k = g.size();
+    while(k > 2) {
+        int i = 0;
+        while(g[i].size() != 1)
+            ++i;
+        int p = g[i][0];
+        ans.push_back(p);
+        *find(g[p].begin(), g[p].end(), i) = g[p].back();
+        g[p].pop_back();
+        g[i].pop_back();
+        k--;
     }
     return ans;
 }
 
-int count_rings(vector<point>& v) {
-    vector<point> v2;
-    int ans = 0;
-    while(!v.empty()) {
-        v2.clear();
-        set<int> discards = graham(v);
-        for(int i = 0; i < v.size(); i++)
-            if(discards.count(i) == 0)
-                v2.push_back(v[i]);
-        v = v2;
-        ans++;
+Graph from_proofer(const vector<int>& proofer_code) {
+    int n = proofer_code.size() + 2;
+    Graph ans(n);
+    vector<bool> used(n, false);
+    for(int i = 0; i < proofer_code.size(); i++) {
+        int j = 0;
+        while(used[j] || find(proofer_code.begin() + i, proofer_code.end(), j) != proofer_code.end())
+            j++;
+        ans[proofer_code[i]].push_back(j);
+        ans[j].push_back(proofer_code[i]);
+        used[j] = true;
     }
+    int x = 0, y = 0;
+    while(used[x])
+        x++;
+    used[x] = true;
+    while(used[y])
+        y++;
+    ans[x].push_back(y);
+    ans[y].push_back(x);
     return ans;
-}
-
-template<typename T, template<typename> typename stack>
-void turnaround(stack<T>& target) {
-    vector<T> tmp;
-    while(target.size() != 0) {
-        tmp.push_back(target.top());
-        target.pop();
-    }
-    for(T& a : tmp)
-        target.push(a);
-}
-template<typename T, template<typename> typename stack>
-void merge(stack<T>& s1, stack<T>& s2, stack<T>& target) {
-    while(s1.size() != 0 && s2.size() != 0) {
-        if(s1.top() > s2.top()) {
-            target.push(s1.top());
-            s1.pop();
-        }
-        else {
-            target.push(s2.top());
-            s2.pop();
-        }
-    }
-    while(s1.size() != 0) {
-        target.push(s1.top());
-        s1.pop();
-    }
-    while(s2.size() != 0) {
-        target.push(s2.top());
-        s2.pop();
-    }
-}
-template<typename T, template<typename> typename stack>
-void merge_sort(stack<T>& s) {
-    int half = s.size() / 2;
-    if(half == 0)
-        return;
-
-    stack<T> left, right;
-    while(s.size() > half) {
-        left.push(s.top());
-        s.pop();
-    }
-    while(s.size() > 0) {
-        right.push(s.top());
-        s.pop();
-    }
-
-    merge_sort(left);
-    merge_sort(right);
-    merge(left, right, s);
-    turnaround(s);
 }
 
 int main() {
@@ -467,20 +327,38 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
+    
+    build_postfixcalc_prereqs();
+    string expr("3.15*abc-sin(x-y/(z-a/b))^ln(n!-(-x+2.5))");
+    string e2 = "-(n!)";
+    cout << calculate_postfix(create_postfix_from_expression(create_terms_from_expression(expr))) << endl;
+    cout << endl;
 
-    cout << find_only_nonsequence_bracket("([()()}]())") << '\n';
-    cout << (does_vector_contain_another<int>({ 1, 2, 3, 4, 5, 6 }, { 1, 3, 5 }) ? "YES\n" : "NO\n");
-
-    stackm<int> st;
-    st.push(1);
-    st.push(4);
-    st.push(5);
-    st.push(2);
-    merge_sort(st);
-    while (st.size() != 0)
-    {
-        cout << st.top() << ' ';
-        st.pop();
+    vector<int> pr = {6, 6, 2, 2, 2};
+    Graph ans = from_proofer(pr);
+    for(int i = 0; i < ans.size(); i++) {
+        cout << i << ": ";
+        for(int j : ans[i])
+            cout << j << ' ';
+        cout << endl;
     }
-    cout << '\n';
+    cout << endl;
+
+    Graph tree = {
+        {1, 4},
+        {0, 2, 3},
+        {1},
+        {1},
+        {0, 5, 6},
+        {4},
+        {4}
+    };
+
+    Graph t2 = from_proofer(to_proofer(tree));
+    for(int i = 0; i < t2.size(); i++) {
+        cout << i << ": ";
+        for(int j : t2[i])
+            cout << j << ' ';
+        cout << endl;
+    }
 }
