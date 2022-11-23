1a18720c7240146d39aa9ec7e4581f06b80d3a15
Khepri <25828025+KDA-dev@users.noreply.github.com>
2022-11-23 15:58:09 +0500

    Update optilab.cpp

--- optilab.cpp
+++ optilab.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 struct Point {
-    double x, y, z;
+    vector<double> coord;
     double selfdot() const {
         return *this * *this;
     }
@@ -15,36 +15,71 @@ struct Point {
         return sqrt(selfdot());
     }
     Point operator+(const Point& other) const {
-        return { x + other.x, y + other.y, z + other.z };
+        vector<double> _coord;
+        for (int i = 0; i < coord.size(); i++)
+            _coord.push_back(coord[i] + other.coord[i]);
+        return { _coord };
     }
     Point operator-(const Point& other) const {
-        return { x - other.x, y - other.y, z - other.z };
+        vector<double> _coord;
+        for (int i = 0; i < coord.size(); i++)
+            _coord.push_back(coord[i] - other.coord[i]);
+        return { _coord };
     }
     Point operator*(double other) const {
-        return { x * other, y * other, z * other };
+        vector<double> _coord;
+        for (int i = 0; i < coord.size(); i++)
+            _coord.push_back(coord[i] * other);
+        return { _coord };
     }
     double operator*(const Point& other) const {
-        return x * other.x + y * other.y + z * other.z;
+        double res = 0;
+        for (int i = 0; i < coord.size(); i++)
+            res += coord[i] * other.coord[i];
+        return res;
     }
     Point operator*(const vector<Point>& matrix) const {
-        vector<double> coord;
-        for (int i = 0; i < 3; i++)
-            coord.push_back(*this * matrix[i]);
-        return { coord[0], coord[1], coord[2] };
+        vector<double> _coord;
+        for (int i = 0; i < coord.size(); i++)
+            _coord.push_back(*this * matrix[i]);
+        return { _coord };
+    }
+};
+typedef vector<Point> matrix;
+
+struct Fn {
+    double operator() (const Point& x) const {
+        return exp(x.coord[0] * x.coord[0] - 2 * x.coord[0]) + x.coord[0] * x.coord[0] 
+            + x.coord[1] * x.coord[1] + exp(x.coord[1] * x.coord[1] + 3 * x.coord[1]);
+    }
+    Point grad(const Point& x) const {
+        double x1 = exp(x.coord[0] * x.coord[0] - 2 * x.coord[0]) * (2 * x.coord[0] - 2) + 2 * x.coord[0];
+        double x2 = 2 * x.coord[1] + exp(x.coord[1] * x.coord[1] + 3 * x.coord[1]) * (2 * x.coord[1] + 3);
+        return { {x1, x2} };
+    }
+    matrix H_rev(const Point& x) const {
+        matrix res(2);
+        double ex = exp(x.coord[0] * x.coord[0] - 2 * x.coord[0]);
+        double ey = exp(x.coord[1] * x.coord[1] + 3 * x.coord[1]);
+        res[0] = { {ex * (2 * x.coord[0] - 2) * (2 * x.coord[0] - 2) + 2 * ex + 2, 0} };
+        res[1] = { {0, 2 + ey * (2 * x.coord[1] + 3) * (2 * x.coord[1] + 3) + 2 * ey} };
+
+        double det = res[0].coord[0] * res[1].coord[1];
+        for (auto& a : res)
+            a = a * (1. / det);
+        swap(res[0].coord[0], res[1].coord[1]);
+        return res;
     }
 };
 
-struct sqrFn {
-    vector<Point> A; // matrix
-    Point b;
-    double c;
-    double operator() (const Point& x) const {
-        return x * A * x * 0.5 + x * b + c;
-    }
-    Point grad(const Point& x) const {
-        return x * A + b;
-    }
-};
+matrix matmult(const matrix& a, const matrix& b) {
+    matrix res(a.size(), { vector<double>(b[0].coord.size(), 0) });
+    for (int i = 0; i < a.size(); i++)
+        for (int j = 0; j < b[0].coord.size(); j++)
+            for (int k = 0; k < b.size(); k++)
+                res[i].coord[j] += a[i].coord[k] * b[k].coord[j];
+    return res;
+}
 
 const double maxalph = 1000, eps = 0.01, dif = 0.002;
 
@@ -59,19 +94,17 @@ double dichotomy(double lb, double rb, function<double(double)> f) {
     return lb + (rb - lb) / 2;
 }
 
-Point FletcherReeves(const Point& start, const sqrFn& f) {
-    Point xk = start, xprev = { 1, 1, 1 }, dk = { 0, 0, 0 };
+Point new_ton(const Point& start, const Fn& f) {
+    Point xk = start;
     int k = 0;
     while (f.grad(xk).len() > eps) {
-        double wprev = f.grad(xk).selfdot() / f.grad(xprev).selfdot();
-        dk = f.grad(xk) + dk * wprev;
+        matrix Hrev = f.H_rev(xk);
 
         double ak = dichotomy(0, maxalph,
-            [&xk, &f, &dk](double t) {
-                return f(xk - dk * t);
+            [&xk, &f, &Hrev](double t) {
+                return f(xk - f.grad(xk) * Hrev * t);
             });
-        xprev = xk;
-        xk = xk - dk * ak;
+        xk = xk - f.grad(xk) * Hrev * ak;
         k++;
     }
     cout << k << '\n';
@@ -80,16 +113,8 @@ Point FletcherReeves(const Point& start, const sqrFn& f) {
 
 int main()
 {
-    sqrFn f10 = {
-        {
-            {2, 1.5, 1},
-            {1.5, 2, 1},
-            {1, 1, 1}
-        },
-        {1, 2, 3},
-        1
-    };
-    auto ans = FletcherReeves({ 1, 1, 1 }, f10);
-    cout << ans.x << ' ' << ans.y << ' ' << ans.z << '\n';
+    Fn f10;
+    auto ans = new_ton({ {0, -5} }, f10);
+    cout << ans.coord[0] << ' ' << ans.coord[1] << '\n';
     cout << f10(ans);
 }
